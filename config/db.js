9fa704f421efcef89dfafaddb42554296d5bb5f7
Jafeth17 <jafethm38@gmail.com>
2024-08-13 19:14:28 -0600

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,6 +2,12 @@ require('dotenv').config();
 
 const { Sequelize } = require('sequelize');
 
+/**
+ * Conexión a la base de datos
+ * 
+ * Esta configuración establece la conexión a la base de datos MySQL utilizando Sequelize.
+ * Los datos de conexión se obtienen de las variables de entorno definidas en el archivo .env.
+ */
 const sequelize = new Sequelize(
     process.env.DB_NAME, 
     process.env.DB_USER, 
